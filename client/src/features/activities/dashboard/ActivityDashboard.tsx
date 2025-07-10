1dbb7dba627eabc76963532555e13e6afa3208ba
Dean Brown <dean@seqteckllc.com>
2025-07-10 16:10:49 -0500

    End of Section 5

---
+++ client/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -0,0 +1,36 @@
+import { Grid2 } from "@mui/material";
+import ActiviltyList from "./ActiviltyList";
+import ActivityDetails from "./details/ActivityDetails";
+import ActivityForm from "../form/ActivityForm";
+
+type Props = {
+    activities: Activity[]
+    selectActivity: (id: string) => void
+    cancelSelectActivity: () => void
+    selectedActivity?: Activity
+    editMode: boolean
+    openForm: (id: string) => void
+    closeForm: () => void
+    submitForm: (activity: Activity) => void
+    deleteActivity: (id: string) => void
+}
+
+export default function ActivityDashboard({activities, cancelSelectActivity, selectActivity, selectedActivity, editMode, openForm, closeForm, submitForm, deleteActivity}: Props) {
+  return (
+    <Grid2 container spacing={3}>
+        <Grid2 size={7}>
+            <ActiviltyList activities={activities} selectActivity={selectActivity} deleteActivity={deleteActivity} />
+        </Grid2>
+        <Grid2 size={5}>
+            {selectedActivity &&  !editMode &&
+              <ActivityDetails 
+                activity={selectedActivity} 
+                cancelSelectActivity={cancelSelectActivity}
+                openForm={openForm}
+
+              />}
+              {editMode && <ActivityForm closeForm={closeForm} activity={selectedActivity} submitForm={submitForm}/>}
+        </Grid2>
+    </Grid2>
+  )
+}
